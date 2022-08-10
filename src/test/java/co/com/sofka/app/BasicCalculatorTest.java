1d9edfbc68c05e926295e04486363066dadcba61
Santiago Sierra <santiagosierrak10@gmail.com>
2022-08-10 16:57:46 -0500

    initial commit

---
+++ src/test/java/co/com/sofka/app/BasicCalculatorTest.java
@@ -0,0 +1,12 @@
+package co.com.sofka.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicCalculatorTest {
+
+    @Test
+    void sum() {
+    }
+}
